cc3b6797218e68cb05dec11ceb99610d67263785
DanielCardonaRojas <d.cardona.rojas@gmail.com>
2016-07-09 13:05:51 -0500

    abstracting over some control structures

--- GenericUtils.hs
+++ GenericUtils.hs
@@ -16,7 +16,9 @@ bothAndMerge  f g h = f &&& g >>> arr (uncurry h)
 (|.|) f g = bothAndMerge f g (||)
 
 compareWith f x y = compare (f x) (f y)
+eqBy f x y = (f x) == (f y)
 
+------------ List operations --------------
 splitEvery _ [] = []
 splitEvery 0 ls = [ls]
 splitEvery n list = first : (splitEvery n rest) 
@@ -25,5 +27,13 @@ splitEvery n list = first : (splitEvery n rest)
 -- | 'innerZip' takes a list and returns a a list of adjacent elements
 -- If the input list has odd length then the last element is trimmed.
 innerZip :: [a] -> [(a,a)]
-innerZip = map tuplify2 . filter ((== 2) . length) . splitEvery 2  
+innerZip = map tuplify . filter ((== 2) . length) . splitEvery 2  where tuplify [x,y] = (x,y)
+
+------------ Conversion ------------
+toMaybe p x = iff p Just (const Nothing)
+toSingleton = maybe [] (:[]) 
+
+------------ Control Combinators  ------------
+-- Ex: iff ((> 3) &.& even) (+ 2) (* 3) 4
+iff p f g = \x -> if p x then f x else g x
 
